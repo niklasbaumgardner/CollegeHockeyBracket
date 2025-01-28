4dde718a44bbb1db4e2d07b18b0f2f8f190fb78f
niklasbaumgardner <niklasbaumgardner@gmail.com>
2025-01-28 17:09:34 -0500

    updating release

--- bracketapp/__init__.py
+++ bracketapp/__init__.py
@@ -33,7 +33,7 @@ sentry_sdk.init(
         # possible.
         "continuous_profiling_auto_start": True,
     },
-    release="nbbracketchallenge@1.0.1",
+    release="nbbracketchallenge@1.0.2",
 )
 
 
