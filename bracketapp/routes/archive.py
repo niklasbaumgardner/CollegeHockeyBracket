3ac4820bc727805aa9d95d7f4ed1562d46f9c7a7
niklasbaumgardner <niklasbaumgardner@gmail.com>
2025-02-21 15:10:05 -0500

    redirect to viewing cbracket when there are no brackets for that year

--- bracketapp/routes/archive.py
+++ bracketapp/routes/archive.py
@@ -1,5 +1,5 @@
 from bracketapp.queries import bracket_queries
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for
 from bracketapp.utils import bracket_utils
 
 
@@ -16,6 +16,9 @@ def archive():
 @archive_bp.get("/archive/<int:year>")
 def archive_year(year):
     standings, winners, correct = bracket_utils.get_bracket_standings_for_year(year)
+
+    if len(standings) == 0:
+        return redirect(url_for("viewbracket_bp.view_cbracket", year=correct.year))
 
     standings_dict = [correct.to_dict(include_games=False)] + [
         b.to_dict(safe_only=False, include_games=False) for b in standings
