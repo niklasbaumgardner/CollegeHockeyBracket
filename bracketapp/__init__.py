083673d1153ecbba15d6ec4b90602532a632c217
niklasbaumgardner <niklasbaumgardner@gmail.com>
2025-02-25 15:22:53 -0500

    Major release 2.0.0: Groups

--- bracketapp/__init__.py
+++ bracketapp/__init__.py
@@ -31,7 +31,7 @@ sentry_sdk.init(
         # possible.
         "continuous_profiling_auto_start": True,
     },
-    release="nbbracketchallenge@1.1.9",
+    release="nbbracketchallenge@2.0.0",
 )
 
 
