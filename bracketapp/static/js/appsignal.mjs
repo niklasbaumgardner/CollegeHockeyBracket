bc6cac02ea96f942c9f837fe04b2cac8f7f4965a
niklasbaumgardner <niklasbaumgardner@gmail.com>
2026-01-27 14:47:46 -0500

    Update Appsignal frontend key

--- bracketapp/static/js/appsignal.mjs
+++ bracketapp/static/js/appsignal.mjs
@@ -2,7 +2,7 @@ import Appsignal from "https://jspm.dev/@appsignal/javascript";
 import { plugin } from "https://jspm.dev/@appsignal/plugin-window-events";
 
 export const appsignal = new Appsignal({
-  key: "d63002ab-9619-420b-8a7f-2fa2ebc5e6e4",
+  key: "124822cf-78b5-4b6b-8105-f525313d458f",
   namespace: "frontend",
   revision: "3.0.0",
 });
