07c6ec7c9d5d4db03d8b23273615178d2be850b1
niklasbaumgardner <niklasbaumgardner@gmail.com>
2026-01-27 17:44:23 -0500

    Update sentry.mjs

--- bracketapp/static/js/sentry.mjs
+++ bracketapp/static/js/sentry.mjs
@@ -1,7 +1,7 @@
 if (window.Sentry) {
   Sentry.onLoad(function () {
     Sentry.init({
-      release: "nbbracketchallengefront@1.0.13",
+      release: "nbbracketchallengefront@3.0.0",
       environment: "production",
       integrations: [Sentry.browserTracingIntegration()],
       tracesSampleRate: 1.0,
