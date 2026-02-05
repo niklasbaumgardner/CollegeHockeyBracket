32d3b6f13cb33beb36d6d2753ccddeba3f3ad977
niklasbaumgardner <niklasbaumgardner@gmail.com>
2026-02-05 10:41:28 -0500

    update gunicorn config

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,13 +1,18 @@
 import os
+import multiprocessing
+
 
 port = os.environ.get("PORT", "3000")
 bind = f"[::]:{port}"
+# bind = "127.0.0.1:5000"
 
-worker_class = "gevent"
-workers = 33
-worker_connections = 1000
+
+cpu_count = multiprocessing.cpu_count()
+print(f"CPU count is {cpu_count}")
+workers = cpu_count + 1
+threads = 6
 
 timeout = 30
 keepalive = 2
 max_requests = 1000
-max_requests_jitter = 50
+max_requests_jitter = 100
