5262c9ba51702e6e52e328616bfb5854bb15bc71
niklasbaumgardner <niklasbaumgardner@gmail.com>
2026-02-25 12:37:34 -0500

    working on caddy

--- caddy/caddy_start.sh
+++ caddy/caddy_start.sh
@@ -6,11 +6,12 @@ set -euo pipefail
 export APP_DOMAIN=${APP_DOMAIN}
 export APP2_DOMAIN=${APP2_DOMAIN}
 export APP_PORT=${APP_PORT}
+export APP2_PORT=${APP2_PORT}
 
 # strip https:// or https:// from domain if necessary
 APP_DOMAIN=${APP_DOMAIN##*://}
 APP2_DOMAIN=${APP2_DOMAIN##*://}
 
-echo using app: ${APP_DOMAIN} and ${APP2_DOMAIN} with port: ${APP_PORT}, listening on ${PORT}
+echo using app: ${APP_DOMAIN} with port: ${APP_PORT} and ${APP2_DOMAIN} with port: ${APP2_PORT}, listening on ${PORT}
 
 exec caddy run --config Caddyfile
