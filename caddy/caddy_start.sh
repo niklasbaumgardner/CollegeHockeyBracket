36b913531f5e61b6a037488b65f4a4fddbf50cfa
niklasbaumgardner <niklasbaumgardner@gmail.com>
2026-02-25 11:53:04 -0500

    update caddy to include chb com

--- caddy/caddy_start.sh
+++ caddy/caddy_start.sh
@@ -4,11 +4,13 @@ set -euo pipefail
 
 # for backwards compatibility, seperates host and port from url
 export APP_DOMAIN=${APP_DOMAIN}
+export APP2_DOMAIN=${APP2_DOMAIN}
 export APP_PORT=${APP_PORT}
 
 # strip https:// or https:// from domain if necessary
 APP_DOMAIN=${APP_DOMAIN##*://}
+APP2_DOMAIN=${APP2_DOMAIN##*://}
 
-echo using app: ${APP_DOMAIN} with port: ${APP_PORT}, listening on ${PORT}
+echo using app: ${APP_DOMAIN} and ${APP2_DOMAIN} with port: ${APP_PORT}, listening on ${PORT}
 
 exec caddy run --config Caddyfile
