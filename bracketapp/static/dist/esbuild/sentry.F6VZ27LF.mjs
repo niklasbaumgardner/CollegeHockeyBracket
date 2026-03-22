fd78d0bc37948232644dcfbe0620294ae023e917
niklasbaumgardner <niklasbaumgardner@gmail.com>
2026-03-22 16:40:39 -0400

    update sentry version

---
+++ bracketapp/static/dist/esbuild/sentry.F6VZ27LF.mjs
@@ -0,0 +1,2 @@
+window.Sentry&&Sentry.onLoad(function(){Sentry.init({release:"nbbracketchallengefront@3.0.2",environment:"production",integrations:[Sentry.browserTracingIntegration()],tracesSampleRate:1,sendDefaultPii:!0})});
+//# sourceMappingURL=sentry.F6VZ27LF.mjs.map
