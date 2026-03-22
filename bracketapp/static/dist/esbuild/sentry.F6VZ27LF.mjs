67f595f010ec2eedd2b68ad0f7dc1db016850c26
niklasbaumgardner <niklasbaumgardner@gmail.com>
2026-03-22 18:23:19 -0400

    update release

--- bracketapp/static/dist/esbuild/sentry.F6VZ27LF.mjs
+++
@@ -1,2 +0,0 @@
-window.Sentry&&Sentry.onLoad(function(){Sentry.init({release:"nbbracketchallengefront@3.0.2",environment:"production",integrations:[Sentry.browserTracingIntegration()],tracesSampleRate:1,sendDefaultPii:!0})});
-//# sourceMappingURL=sentry.F6VZ27LF.mjs.map
---
+++ bracketapp/static/dist/esbuild/sentry.TZFUPKOS.mjs
@@ -0,0 +1,2 @@
+window.Sentry&&Sentry.onLoad(function(){Sentry.init({release:"nbbracketchallengefront@3.0.3",environment:"production",integrations:[Sentry.browserTracingIntegration()],tracesSampleRate:1,sendDefaultPii:!0})});
+//# sourceMappingURL=sentry.TZFUPKOS.mjs.map
