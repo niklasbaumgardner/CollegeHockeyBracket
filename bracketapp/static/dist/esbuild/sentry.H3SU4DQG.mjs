456c09ec160e94d3563384a3e7e2ba9aa9724133
niklasbaumgardner <niklasbaumgardner@gmail.com>
2026-03-25 17:05:06 -0400

    typo

---
+++ bracketapp/static/dist/esbuild/sentry.H3SU4DQG.mjs
@@ -0,0 +1,2 @@
+window.Sentry&&Sentry.onLoad(function(){Sentry.init({release:"nbbracketchallengefront@3.0.23",environment:"production",integrations:[Sentry.browserTracingIntegration()],tracesSampleRate:1,sendDefaultPii:!0})});
+//# sourceMappingURL=sentry.H3SU4DQG.mjs.map
--- bracketapp/static/dist/esbuild/sentry.XD4WS6HU.mjs
+++
@@ -1,2 +0,0 @@
-window.Sentry&&Sentry.onLoad(function(){Sentry.init({release:"nbbracketchallengefront@3.0.22",environment:"production",integrations:[Sentry.browserTracingIntegration()],tracesSampleRate:1,sendDefaultPii:!0})});
-//# sourceMappingURL=sentry.XD4WS6HU.mjs.map
